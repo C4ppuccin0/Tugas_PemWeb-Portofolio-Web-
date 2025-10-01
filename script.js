8d6ffc3deb2f2c90d04133354adb3e649098dc4d
Argha <niqiewijaksono1106@gmail.com>
2025-10-01 20:53:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('nav .nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 80, 
+        behavior: "smooth"
+      });
+    }
+    navLinks.classList.remove('active');
+  });
+});
